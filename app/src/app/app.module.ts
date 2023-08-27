3fe0f5f227eee8c1e2ade8369783c2b8d2dbf052
Marley <marley.nodejs@gmail.com>
2023-08-27 15:44:11 +0300

    07. Advantage of unsubscribing the http observable in ngOnDestroy

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PostsComponent } from './pages/posts/posts.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  declarations: [AppComponent, HeaderComponent, PostsComponent],
+  declarations: [AppComponent, HeaderComponent, PostsComponent, HomeComponent],
 
   providers: [],
   bootstrap: [AppComponent],
