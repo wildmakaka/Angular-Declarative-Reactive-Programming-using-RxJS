e0b401ff52b8dffa9b9d7aa4421e3247ed072406
Marley <marley.nodejs@gmail.com>
2023-08-30 19:29:17 +0300

    20. Design Add Post Form using Reactive Forms Module

--- app/src/app/components/add-post/add-post.component.ts
+++ app/src/app/components/add-post/add-post.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { DeclarativeCategoryService } from 'src/app/services/DeclarativeCategory.service';
 
 @Component({
   selector: 'app-add-post',
   templateUrl: './add-post.component.html',
-  styleUrls: ['./add-post.component.scss']
+  styleUrls: ['./add-post.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AddPostComponent {
+  postForm = new FormGroup({
+    title: new FormControl(''),
+    description: new FormControl(''),
+    categoryId: new FormControl(''),
+  });
 
-}
+  categories$ = this.categoryService.categories$;
+
+  constructor(private categoryService: DeclarativeCategoryService) {}
+
+  onAddPost() {
+    console.log(this.postForm.value);
+  }
+} // The End of Class;
